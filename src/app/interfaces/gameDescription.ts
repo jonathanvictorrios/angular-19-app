1e087d8c51aba492bb2ea84b365832fc7f417c59
jonavictorrios <jonathan.rios-ls@alitaware.com>
2024-12-21 20:01:57 -0300

    it was added interfaces for games array

---
+++ src/app/interfaces/gameDescription.ts
@@ -0,0 +1,11 @@
+//here we can define the types of variables of gameDescription using a Interface (this is a feature of typescript lenguage)
+export interface gameDescription {
+  data: Array<description>;
+  //Array<description> it´s mean an array where each element is an object of interfaces called description.
+}
+interface description{
+  id:number,
+  name:string,
+  genre:string,
+  year:number
+}
